3e92032e8457b40252899364ab4346bad3a5e04b
SHARAD RAJ SINGH MAURYA <36638057+cwithsharad@users.noreply.github.com>
2018-06-09 17:47:35 +0530

    commit

--- c_projects/bms/bms.c
+++ c_projects/bms/bms.c
@@ -4,49 +4,52 @@
 #include<malloc.h>
 #include<stdlib.h>
 
-#define HS printf("\t\t\t\t\t#########################################################\n");
-#define LI printf("\n\t\t\t\t\t=========================================================\n\n");
-#define NLI printf("\n\n\n\n\n\t\t\t\t\t========================================================\n\n");
-#define SSP printf("\t\t\t\t\t");
+/* MACRO DEFINITIONS FOR PRINTING LINENE AND TABS FOR EASE OF USE */
+
+#define HLINE printf("\t\t\t\t\t#########################################################\n");
+#define LINE printf("\n\t\t\t\t\t=========================================================\n\n");
+#define NEW printf("\n\n\n\n\n\t\t\t\t\t========================================================\n\n");
+#define SIDETAB printf("\t\t\t\t\t");
 #define CLS system("cls");
 
+/* FILE POINTER */
 
-int new_ac(void);         //     FOR ADDING A NEW ACCOUNT          
-int clo_ac(void);         //     FOR CLOSING AN ACCOUNT            
-int dr(void);             //     FOR DEBITING AN ACCOUNT           
-int cr(void);             //     FOR CREDITING AN ACCOUNT          
-int sd(long double);      //     FOR SHOWING DETAINS OF AN ACCOUNT 
+int new_ac(void);         /*     FOR ADDING A NEW ACCOUNT           */
+int clo_ac(void);         /*     FOR CLOSING AN ACCOUNT             */
+int dr(void);             /*     FOR DEBITING AN ACCOUNT            */
+int cr(void);             /*     FOR CREDITING AN ACCOUNT           */
+int sd(long double);      /*     FOR SHOWING DETAINS OF AN ACCOUNT  */
 
 // GLOBAL VARIABLES
 
-long double sub_bal;           // FOR STORING THE BALANCE DEPOSITED
-char h_name[30],h_ads[100];    // FOR STORING THE HOLDER NAME AND ADDRESS
-long double ac_no;             // FOR STORING THE ACCOUNT NUMBER
-static int flag=0;             /*  TO TEST THE CONITION THAT THE FUNCTION sd() 
+long double sub_bal;           /* FOR STORING THE BALANCE DEPOSITED             */
+char h_name[30],h_ads[100];    /* FOR STORING THE HOLDER NAME AND ADDRESS       */
+long double ac_no;             /* FOR STORING THE ACCOUNT NUMBER                */
+static int flag=0;             /*  TO TEST THE CONITION THAT THE FUNCTION sd()
                                   IS CALLED FROM THE new_ac() FUNCTION OR NOT   */
 
 
 /* MAIN FUNCTION HAS ONLY THE INTERFACE AND THE SWITCH STATEMENTS FOR CHOOSING
    THE OPTIONS AFTER CHOOSING THE OPTIONS THE RESPECTED FUNCTIONS ARE CALLED   */
 
-// MAIN FUNCTION STARTS
+/* MAIN FUNCTION STARTS */
 
 int main()
 {
     flag=0;
-    system("color F0");
+    system("color a");
     int op;
     options:
-    NLI
+    NEW
     printf("\t\t\t\t\t\t WELCOME TO X BANK MANAGEMENT SOFTWARE\n");
-    LI
+    LINE
     printf("\t\t\t\t\tENTER THE CHOICE FROM THE OPTIONS BELOW AND PRESS ENTER \n");
-    LI
+    LINE
     printf("\t\t\t\t\t1: ADD OR REMOVE ACCOUNT\n");
-    printf("\t\t\t\t\t2: DEBIT ACCOUNT OR CREDIR ACCOUNT\n");
+    printf("\t\t\t\t\t2: DEBIT ACCOUNT OR CREDIT ACCOUNT\n");
     printf("\t\t\t\t\t3: SHOW DETAILS OF ACCOUNT\n");
     printf("\t\t\t\t\t4: MODIFY AN ACCOUNT\n\n");
-    HS
+    HLINE
     printf("\t\t\t\t\t0: EXIT \n\n");
     printf("\n\t\t\t\t\tENTER YOUR CHOICE : ");
     scanf("%d",&op);
@@ -56,12 +59,12 @@ int main()
         {
             int ac_choice;
             system("cls");
-            NLI
+            NEW
             printf("\t\t\t\t\tENTER THE CHOICE FROM THE OPTIONS BELOW AND PRESS ENTER \n");
-            LI
-            SSP
+            LINE
+            SIDETAB
             printf("1: ADD AN ACCOUNT\n");
-            SSP
+            SIDETAB
             printf("2: REMOVE AN ACCOUNT\n");
             printf("\n\t\t\t\t\tENTER YOUR CHOICE : ");
             scanf("%d",&ac_choice);
@@ -76,12 +79,12 @@ int main()
         {
             int drcr_choice;
             system("cls");
-            NLI
+            NEW
             printf("\t\t\t\t\tENTER THE CHOICE FROM THE OPTIONS BELOW AND PRESS ENTER \n");
-            LI
-            SSP
+            LINE
+            SIDETAB
             printf("1: DEBIT AN ACCOUNT\n");
-            SSP
+            SIDETAB
             printf("2: CREDIT AN ACCOUNT\n");
             printf("\n\t\t\t\t\tENTER YOUR CHOICE : ");
             scanf("%d",&drcr_choice);
@@ -98,10 +101,10 @@ int main()
         {
             long double srch_ac;
             system("cls");
-            NLI
+            NEW
             printf("\t\t\t\t\tENTER THE ACCOUNT NUMBER BELOW AND PRESS ENTER\n");
-            LI
-            SSP
+            LINE
+            SIDETAB
             printf("\n\t\t\t\t\tENTER ACCOUNT NUMBER : ");
             scanf("%lf",&srch_ac);
             sd(srch_ac);
@@ -112,10 +115,10 @@ int main()
         {
             long double mod_ac;
             system("cls");
-            NLI
+            NEW
             printf("\t\t\t\t\t     ENTER THE ACCOUNT NUMBER BELOW AND PRESS ENTER\n");
-            LI
-            SSP
+            LINE
+            SIDETAB
             printf("\n\t\t\t\t\tENTER ACCOUNT NUMBER TO MODIFY : ");
             scanf("%lf",&mod_ac);
             sd(mod_ac);
@@ -125,12 +128,12 @@ int main()
     case 0:
         {
             CLS
-            NLI
+            NEW
             printf("\t\t\t\t\t\t     X  BANK MANAGEMENT SOFTWARE\n\n");
             printf("\t\t\t\t\t\t   MADE BY : SHARAD RAJ SINGH MAURYA\n");
             printf("\t\t\t\t\t       CREDITS : MYSELF BEACUSE I MADE IT ALONE :)\n");
-            LI
-            SSP
+            LINE
+            SIDETAB
             printf("EXITING .................................................\n\n");
             exit(0);
         }
@@ -138,99 +141,99 @@ int main()
     }
     return 0;
 }
-// END OF main() FUNCTION
+/* END OF main() FUNCTION */
 
 
-//FUNCTION FOR MAKING NEW ACCOUNT STARTS
+/* FUNCTION FOR MAKING NEW ACCOUNT STARTS */
 
 int new_ac(void)
 {
     system("cls");
-    NLI
+    NEW
     printf("\t\t\t\t\t\tENTER THE DETAILS BELOW AND PRESS ENTER\n");
-    LI
-    SSP
+    LINE
+    SIDETAB
     getchar();
     printf("ENTER ACCOUNT HOLDER NAME : ");
     gets(h_name);
-    SSP
+    SIDETAB
     printf("ENTER ACCOUNT NUMBER : ");
     scanf("%lf",&ac_no);
-    SSP
+    SIDETAB
     getchar();
     printf("ENTER ADDRESS : ");
     gets(h_ads);
-    SSP
+    SIDETAB
     printf("ENTER BALANCE : ");
     scanf("%lf",&sub_bal);
     flag=1;
     sd(ac_no);
 }
-//FUNCTION FOR MAKING NEW ACCOUNT  ENDS
+/* FUNCTION FOR MAKING NEW ACCOUNT  ENDS */
 
 
-// FUNCTION FOR DELETING AN ACCOUNT STARTS
+/* FUNCTION FOR DELETING AN ACCOUNT STARTS */
 
 int clos_ac(void)
 {
     system("cls");
-    NLI
-}                               
-// FUNCTION FOR DELETING AN ACCOUNT ENDS
+    NEW
+}
+/* FUNCTION FOR DELETING AN ACCOUNT ENDS */
 
 
-// FUNCTION FOR DEBITING AN ACCOUNT STARTS
+/* FUNCTION FOR DEBITING AN ACCOUNT STARTS */
 
 int dr(void)
 {
     system("cls");
-    NLI
+    NEW
 }
-// FUNCTION FOR DEBITING AN ACCOUNT ENDS
+/* FUNCTION FOR DEBITING AN ACCOUNT ENDS */
 
 
-//FUNCTION FOR CREDITING AN ACCOUNT  STARTS
+/* FUNCTION FOR CREDITING AN ACCOUNT  STARTS */
 
 int cr(void)
 {
     system("cls");
-    NLI
+    NEW
 }
-//FUNCTION FOR CREDITING AN ACCOUNT ENDS 
+/* FUNCTION FOR CREDITING AN ACCOUNT ENDS */
 
 
-//FUNCTION FOR SHOWING DETAILS OF AN ACCOUNT STARTS
+/* FUNCTION FOR SHOWING DETAILS OF AN ACCOUNT STARTS */
 
 int sd(long double search_ac)
 {
     system("cls");
-    NLI
+    NEW
     if(flag==1)
-        {SSP
+        {SIDETAB
         printf("\t   ACCOUNT MADE SUCCESSFULLY !!! \n");
         ac_no=search_ac;
         }
     if(flag==2)
-        {SSP
+        {SIDETAB
         printf("\   ACCOUNT MODIFIED SUCCESSFULLY !!! \n");
         ac_no=search_ac;
         }
     else if(flag==0)
     printf("\t\t\t\t\tDETAILS OF ACCOUNT : %.0lf \n",ac_no);
-    LI
-    HS
-    SSP
+    LINE
+    HLINE
+    SIDETAB
     printf("ACCOUNT HOLDER NAME : %s\n",h_name);
-    SSP
+    SIDETAB
     printf("ACCOUNT NUMBER : %.0lf\n",ac_no);
-    SSP
+    SIDETAB
     printf("ADDRESS : %s\n",h_ads);
-    SSP
+    SIDETAB
     printf("BALANCE : %f\n",sub_bal);
-    HS
+    HLINE
     getchar();
     getchar();
     CLS
     main();
 }
-//FUNCTION FOR SHOWING DETAILS OF AN ACCOUNT ENDS
+/* FUNCTION FOR SHOWING DETAILS OF AN ACCOUNT ENDS */
